75384073013de902d6da3b3b4e59b9b645bdde1f
abko <abko121115@gmail.com>
2025-11-20 11:20:01 +0000

    feat: DeleteTodoById 엔드포인트에서 삭제 로직을 개선하여 ExecuteDeleteAsync 사용

--- TodoList/Features/Todos/DeleteTodoById/Endpoint.cs
+++ TodoList/Features/Todos/DeleteTodoById/Endpoint.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.EntityFrameworkCore;
 using TodoList.Data;
 
 namespace TodoList.Features.Todos.DeleteTodoById
@@ -7,11 +8,21 @@ namespace TodoList.Features.Todos.DeleteTodoById
     {
         public static async Task<Results<Ok, NotFound>> HandleAsync(int id, AppDbContext context)
         {
-            var todo = await context.Todos.FindAsync(id);
-            if (todo is null) return TypedResults.NotFound();
 
-            context.Todos.Remove(todo);
-            await context.SaveChangesAsync();
+            // 첫 번째 방법
+            // var todo = await context.Todos.FindAsync(id);
+            // if (todo is null) return TypedResults.NotFound();
+
+            // context.Todos.Remove(todo);
+            // await context.SaveChangesAsync();
+            // return TypedResults.Ok();
+
+            // 두 번째 방법
+            var affected = await context.Todos
+                .Where(t => t.Id == id)
+                .ExecuteDeleteAsync();
+
+            if (affected == 0) return TypedResults.NotFound();
             return TypedResults.Ok();
         }
     }
